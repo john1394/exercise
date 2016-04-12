25f5fc30b9bc8bb7ab0f18bdf70998a14e6ea29b
yibo <yibo@feinno.com>
2016-04-12 20:53:45 +0800

    refactor

--- exercise-basics/exercise-basics/src/src/test/java/com/somecompany/exercise/SampleTest.java
+++ exercise-basics/exercise-basics/src/src/test/java/com/somecompany/exercise/SampleTest.java
@@ -2,13 +2,18 @@ package com.somecompany.exercise;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SampleTest {
-	@Test
-	public void test1() 
-	{
-		SampleArg a=new SampleArg();	
-		//
-		System.out.println(a);
-		
-	}
+    @Test
+    public void test1() {
+        SampleArg a = new SampleArg();
+        //
+        List<String> b = new ArrayList<String>();
+        b.add("aa");
+        b.stream().forEach(System.out::println);
+        System.out.println(a);
+
+    }
 }
